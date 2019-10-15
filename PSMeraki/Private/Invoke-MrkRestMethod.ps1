b6b1a149e552ca63cc369d5a1641f3b033480ad8
envegter <47079538+envegter@users.noreply.github.com>
2019-10-15 09:46:09 +0000

    upadated Invoke-mrkRestMethod to assume UTF8, update Update-MrkNetworkMXL7FwRule to support set mrkConfigState

--- PSMeraki/Private/Invoke-MrkRestMethod.ps1
+++ PSMeraki/Private/Invoke-MrkRestMethod.ps1
@@ -40,14 +40,14 @@ function Invoke-MrkRestMethod {
         if ($_.exception.message -match [regex]::Escape('(429)')){
             Write-Verbose "Meraki reports 'Too many API Rquests', sleeping 1 second and rerunning the same request"
             Start-Sleep 1
-             $request = Invoke-RestMethod -Method $Method -ContentType 'application/json' -Headers $mrkRestApiHeader -Uri $uri -Body ($body | ConvertTo-Json -Depth 10)
+             $request = Invoke-RestMethod -Method $Method -ContentType 'application/json; charset=utf-8' -Headers $mrkRestApiHeader -Uri $uri -Body ($body | ConvertTo-Json -Depth 10)
             # Invoke-MrkRestMethod -ResourceID $ResourceID -Method $method -body $body;
         } elseif ($_.exception.message -match [regex]::Escape('(308)')){
              Write-Verbose "Meraki reports redirection. Request the orgBaseUri and rerun the same request"
              $global:orgBaseUri = $null
              Get-MrkOrgEndpoint # reset the $global:orgBaseUri variable to get the non-default api.meraki.com URI
              $uri = $global:orgBaseUri + $ResourceID
-             $request = Invoke-RestMethod -Method $Method -ContentType 'application/json' -Headers $mrkRestApiHeader -Uri $uri -Body ($body | ConvertTo-Json -Depth 10)
+             $request = Invoke-RestMethod -Method $Method -ContentType 'application/json; charset=utf-8' -Headers $mrkRestApiHeader -Uri $uri -Body ($body | ConvertTo-Json -Depth 10)
         } else {
             Get-RestError
         }
