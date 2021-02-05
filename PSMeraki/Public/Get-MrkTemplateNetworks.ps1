29280b45bb5f45cb21af64e021a284c2b27208e6
merriry <ryan@merrill-johnson.com>
2021-02-05 11:33:53 -0700

    Add functions
    Add functions to look up devices bound to templates and unbind devices from templates

---
+++ PSMeraki/Public/Get-MrkTemplateNetworks.ps1
@@ -0,0 +1,33 @@
+function Get-MrkTemplateNetworks {
+    <#
+    .SYNOPSIS
+    Retrieves all meraki networks with either a bound Template or a matching template ID
+    .DESCRIPTION
+    Retrieves all meraki networks for an organization bound to a template or a single template if the templateId is provided
+    .EXAMPLE
+    Get-MrkTemplateNetworks 
+    .EXAMPLE
+    Get-MrkTemplateNetworks -OrgId 111222
+    .EXAMPLE
+    Get-MrkTemplateNetworks -templateId L_615304299089509767
+    .PARAMETER orgId
+    optional parameter specify an OrgId, default it will take the first OrgId retrieved from Get-MrkOrganizations
+    .PARAMETER TemplateId
+    optional parameter to specify a specific configTemplateID. retrieve a specific set of networks bound to a template, use get-mrkTemplate 
+    and then use the retrieved id to get networks from that specific template
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter()][String]$orgId = (Get-MrkFirstOrgID),
+        [Parameter()][String]$templateId
+    )
+    if($null -eq $templateId -or "" -eq $templateId){
+        #{{baseUrl}}/organizations/{{organizationId}}/networks
+        $request = Invoke-MrkRestMethod -Method GET -ResourceID ('/organizations/' + $orgId + '/networks')
+        $request = $request | Where-Object {$null -ne $_.configTemplateID}
+    } else {
+        #{{baseUrl}}/organizations/{{organizationId}}/networks?configTemplateId={{templateId}}
+        $request = Invoke-MrkRestMethod -Method GET -ResourceID ('/organizations/' + $orgId + '/networks?configTemplateId=' + $templateId);
+    }
+    return $request
+}
---
+++ PSMeraki/Public/Remove-MrkNetworkTemplate.ps1
@@ -0,0 +1,18 @@
+function Remove-MrkNetworkTemplate {
+    <#
+    .SYNOPSIS
+    Unbinds a device from a template
+    .DESCRIPTION
+    blah 
+    .EXAMPLE
+    Remove-MrkNetworkTemplate -networkId X_111122223639801111
+    .PARAMETER networkId 
+    id of a network (get-MrkNetworks)[0].id
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)][ValidateNotNullOrEmpty()][String]$networkId
+    )
+    $request = Invoke-MrkRestMethod -Method POST -ResourceID ('/networks/' + $networkId + '/unbind/')
+    return $request
+}
