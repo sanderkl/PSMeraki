870155e38310fb39b40b39cf7ff23961e3cf4810
merriry <ryan@merrill-johnson.com>
2021-02-17 16:50:28 -0700

    fix endpoint

--- PSMeraki/Public/Get-MrkOrgEndpoint.ps1
+++ PSMeraki/Public/Get-MrkOrgEndpoint.ps1
@@ -10,7 +10,7 @@ function Get-MrkOrgEndpoint {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     if (!$orgBaseUri){
         Write-Verbose "Get-MrkOrgEndpoint: global orgBaseUri is empty, retrieving organization specific meraki endpoint"
-        $orgURI = 'https://api.meraki.com/api/v1/organizations'
+        $orgURI = 'https://api.meraki.com/api/v0/organizations'
         $webRequest = Invoke-WebRequest -uri $orgURI -Method GET -Headers (Get-MrkRestApiHeader)
         $redirectedURL = $webRequest.BaseResponse.ResponseUri.AbsoluteUri
         $script:orgBaseUri = $redirectedURL.Replace('/organizations','')
